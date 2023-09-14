935cb85e90c1184a0ccef852f2fcea6797054042
Savannah Acevedo <140823774+savvycolleenusers.noreply.github.com>
2023-09-14 13:35:03 -0400

    added remove-exclamation-marks file

---
+++ codewars/8kyu/remove-exclamation-marks.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//My Solution
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, "");
+}
