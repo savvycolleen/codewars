8d4d308995ee771d4e8ded113a9243aafa83b091
Savannah Acevedo <140823774+savvycolleenusers.noreply.github.com>
2023-09-14 13:22:06 -0400

    added return-negative file

---
+++ codewars/8kyu/return-negative.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+//My Solution
+
+function makeNegative(num) {
+  return -Math.abs(num)
+}
