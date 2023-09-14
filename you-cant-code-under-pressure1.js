1f50839231071109dcd8e5102239efc52c6f6794
Savannah Acevedo <140823774+savvycolleen@users.noreply.github.com>
2023-09-14 11:51:52 -0400

    Delete you-cant-code-under-pressure1.js

--- you-cant-code-under-pressure1.js
+++
@@ -1,9 +0,0 @@
-// DESCRIPTION:
-// Code as fast as you can! You need to double the integer and return it.
-
-//My solution
-
-function doubleInteger(i) {
-  // i will be an integer. Double it and return it.
-  return i * 2;
-}
