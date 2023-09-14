8b0011ce5ba0618f2ffb8a64766f266c372ad79b
Savannah Acevedo <140823774+savvycolleenusers.noreply.github.com>
2023-09-14 13:32:55 -0400

    added returning-strings file

---
+++ codewars/8kyu/returning-strings.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//My Solution
+
+function greet(name){
+  let greeting = `Hello, ${name} how are you doing today?`
+  return greeting
+}
