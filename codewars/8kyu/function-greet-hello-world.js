2f484dd2ef418e177211a97d45ad62ed00bbf659
Savannah Acevedo <140823774+savvycolleenusers.noreply.github.com>
2023-09-14 12:55:49 -0400

    added function-greet-hello-world file

---
+++ codewars/8kyu/function-greet-hello-world.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+//My Solution
+
+// Write a function "greet" that returns "hello world!"
+function greet(){
+  return "hello world!"
+}
