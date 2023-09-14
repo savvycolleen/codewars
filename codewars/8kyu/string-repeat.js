56f7c19259bc3a270e14ca42bc273dde6cf53039
Savannah Acevedo <140823774+savvycolleenusers.noreply.github.com>
2023-09-14 13:24:55 -0400

    added string-repeat file

---
+++ codewars/8kyu/string-repeat.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+//My Solution
+
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
